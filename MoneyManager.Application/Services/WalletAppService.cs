ecd0802ec655f733a0b9fdf9cfc987da4042c0db
npoleva <poleva.2002@mail.ru>
2025-11-05 15:17:54 +0300

    chore: delete block-comment

--- MoneyManager.Application/Services/WalletAppService.cs
+++ MoneyManager.Application/Services/WalletAppService.cs
@@ -1,106 +1,3 @@
-/*using MoneyManager.Application.Dto;
-using MoneyManager.Domain.Aggregates;
-using MoneyManager.Domain.Entities;
-using MoneyManager.Domain.RepositoryInterfaces;
-using MoneyManager.Domain.ValueObjects;
-
-namespace MoneyManager.Application.Services;
-
-public class TransactionsByTypeDto
-{
-    public string Type { get; set; } = null!;
-    public List<TransactionDto> Transactions { get; set; } = new();
-    public decimal TotalAmount { get; set; }
-}
-public class WalletAppService
-{
-    private readonly IWalletRepository _repository;
-
-    public WalletAppService(IWalletRepository repository)
-    {
-        _repository = repository;
-    }
-
-    public WalletDto CreateWallet(string name, decimal initialBalance, string currency)
-    {
-        var wallet = new Wallet(name, new Money(initialBalance, currency));
-        _repository.Save(wallet);
-        return new WalletDto
-        {
-            Id = wallet.Id,
-            Name = wallet.Name,
-            InitialBalance = wallet.InitialBalance.Amount,
-            Currency = wallet.InitialBalance.Currency
-        };
-    }
-
-    public void AddTransaction(Guid walletId, TransactionDto dto)
-    {
-        var wallet = _repository.GetById(walletId);
-
-        if (dto.Type == "Expense" && wallet.CurrentBalance.Amount < dto.Amount)
-            throw new InvalidOperationException("Недостаточно средств для этой траты.");
-
-        var transaction = new Transaction(
-            new Money(dto.Amount, dto.Currency),
-            dto.Type == "Income" ? TransactionType.Income : TransactionType.Expense,
-            new TransactionDescription(dto.Description),
-            DateTime.UtcNow
-        );
-
-        wallet.AddTransaction(transaction);
-        _repository.AddTransaction(transaction);
-    }
-
-    private List<Transaction> GetTransactionsByMonthAndYearAscending(Guid walletId, int month, int year)
-    {
-        var wallet = _repository.GetById(walletId);
-        return wallet.Transactions
-            .Where(t => t.Date.Month == month && t.Date.Year == year)
-            .OrderBy(t => t.Date)
-            .ToList();
-    }
-
-    public List<Transaction> GetTopExpensesByMonthAndYearDescending
-        (Guid walletId, int month, int year, int topCount = 3)
-    {
-        var transactions = GetTransactionsByMonthAndYearAscending(walletId, month, year);
-
-        return transactions
-            .Where(t => t.Type == TransactionType.Expense)
-            .OrderByDescending(t => t.Amount.Amount)
-            .Take(topCount)
-            .ToList();
-    }
-    public List<TransactionsByTypeDto> GetTransactionsGroupedByTypeDesc(Guid walletId, int month, int year)
-    {
-        var transactions = GetTransactionsByMonthAndYearAscending(walletId, month, year)
-            .Select(t => new TransactionDto(
-                t.Type.ToString(),
-                t.Amount.Amount,
-                t.Amount.Currency,
-                t.Description.Value,
-                t.Date))
-            .ToList();
-
-        var grouped = transactions
-            .GroupBy(t => t.Type)
-            .Select(g => new TransactionsByTypeDto
-            {
-                Type = g.Key,
-                Transactions = g.OrderBy(t => t.Date).ToList(),
-                TotalAmount = g.Sum(t => t.Amount)
-            })
-            .OrderByDescending(g => g.TotalAmount)
-            .ToList();
-
-        return grouped;
-    }
-
-
-    public List<Wallet> GetAllWallets() => _repository.GetAll();
-}*/
-
 using MoneyManager.Application.Dto;
 using MoneyManager.Domain.Aggregates;
 using MoneyManager.Domain.Entities;
@@ -150,8 +47,8 @@ public class WalletAppService
             DateTime.UtcNow
         );
 
-        wallet.AddTransaction(transaction); // бизнес-правила внутри агрегата
-        _repository.AddTransaction(transaction); // сохраняем транзакцию
+        wallet.AddTransaction(transaction); 
+        _repository.AddTransaction(transaction); 
     }
 
     private List<Transaction> GetTransactionsByMonthAndYearAscending(Guid walletId, int month, int year)
